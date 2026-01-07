e2b432b2183a42c1ff89e1b182d0ecdbdc54f2ab
Levi Althaus <levialthaus@student.unsw.edu.au>
2026-01-06 23:01:54 -0500

    Yossi's Review

--- src/components/testimonials/myTestimonials.ts
+++ src/components/testimonials/myTestimonials.ts
@@ -8,4 +8,11 @@ export const myTestimonials: TestimonialCardProps[] = [
     rating: 4,
     imagePath:"/images/avatar3.jpeg"
  },
+ {
+    name: "Yossi Althaus",
+    role: "CEO, SigmaCorp",
+    content: "Very creative and exceptional quality. Top G! Very useful.",
+    rating: 5,
+    imagePath:"/images/yossi.HEIC"
+ },
 ]
